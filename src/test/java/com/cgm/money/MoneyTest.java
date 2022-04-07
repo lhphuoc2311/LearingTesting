a80d8a66bd0623d68fd1a0760a6a4e443a78e73d
lhphuoc2311 <phuoc2311@gmail.com>
2022-04-07 16:46:39 +0700

    get rid of duplication (removed subclass Dollar, Franc)

--- src/test/java/com/cgm/money/MoneyTest.java
+++ src/test/java/com/cgm/money/MoneyTest.java
@@ -12,46 +12,37 @@ import org.junit.jupiter.api.Test;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
+    void testMultiplication() {
 
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
-        
+
+        Money fiveF = Money.franc(5);
+        assertEquals(Money.franc(10), fiveF.times(2));
+        assertEquals(Money.franc(15), fiveF.times(3));
+
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
 
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
-        assertNotEquals(Franc.franc(8), Money.dollar(5));
-        
+        assertNotEquals(Money.franc(8), Money.dollar(5));
+
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
+
     }
 
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-        
-    }
-    @Test
-    void testEqualityFranc() {
-        assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(8));
-        
-    }
 
     @Test
     void testCurrency() {
 
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-        
+
     }
 
-    
-    
-    
 }
